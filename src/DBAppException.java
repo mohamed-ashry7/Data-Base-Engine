642a129be8374c83a12639f3382d55cac1b4f3a8
IbrahimMohammed47 <ibrahim.mohammed@student.guc.edu.eg>
2019-03-02 02:50:19 +0200

    DBAppException is complete

--- src/DBAppException.java
+++ src/DBAppException.java
@@ -1,4 +1,12 @@
 
 public class DBAppException extends Exception{
-
+    String str1;
+    
+	DBAppException(String str2) {
+		str1=str2;
+	}
+	
+	public String toString(){ 
+		return ("DBAppException Occurred: "+str1) ;
+	}
 }
